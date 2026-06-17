2f663fc91badbf380b6de70a1faed66152030ad4
abdelhakamami <abdelhakamami@gmail.com>
2026-06-17 12:08:53 +0100

    fix

--- App_Registration/Development/main.tf
+++ App_Registration/Development/main.tf
@@ -1,6 +1,6 @@
 resource "azuread_application" "app-Registration" {
   display_name = var.appregistration-name
-  
+
   # Optional: Add redirect URIs for OAuth2 or OpenID Connect
   web {
     redirect_uris = [var.appregistration_web_redirect_uris]
@@ -15,5 +15,5 @@ resource "azuread_service_principal" "sp-appregistration" {
 resource "azuread_application_password" "client-secret-appregistration" {
   application_id = azuread_application.app-Registration.application_id
   display_name   = "MyAppSecret"
-  end_date_relative = "8760h" 
+  end_date_relative = "8760h"
 }
--- App_Registration/Development/provider.tf
+++ App_Registration/Development/provider.tf
@@ -7,13 +7,13 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.0"  
+      version = "~> 2.0"
     }
   }
   backend "azurerm" {
     resource_group_name  = "storing-state-storageaccount"
     storage_account_name = "devstoringavaxia"
     container_name       = "tfstate-app-registraion-dev"
-    key                  = "tfstate-app-registraion-dev"  # Update this for every state for example new creation of a new container 
+    key                  = "tfstate-app-registraion-dev"  # Update this for every state for example new creation of a new container
   }
 }

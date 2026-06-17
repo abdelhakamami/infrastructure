aedba5f7ab306b982a5fd5367329557c7653fe7f
abdelhakamami <abdelhakamami@gmail.com>
2026-06-17 12:43:09 +0100

    fix

--- Virtual_Network/Development/main.tf
+++ Virtual_Network/Development/main.tf
@@ -1,21 +1,12 @@
-data "terraform_remote_state" "resource_group_state" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = var.rg_tf_state_rg_name
-    storage_account_name = var.rg_tf_state_storage_account_name
-    container_name       = var.rg_tf_state_container_name
-    key                  = var.rg_tf_state_key 
-  }
-}
 resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   address_space       = [var.address_space]
   location            = var.location
-  resource_group_name = data.terraform_remote_state.resource_group_state.outputs.resource_group_name
+  resource_group_name = var.vnet_rg_name
 }
 resource "azurerm_subnet" "subnetA" {
   name                 = var.azurerm_subnetA
-  resource_group_name  = data.terraform_remote_state.resource_group_state.outputs.resource_group_name
+  resource_group_name  = var.vnet_rg_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.azurerm_subnet_subnetA_address_prefixes]
   depends_on = [
@@ -24,7 +15,7 @@ resource "azurerm_subnet" "subnetA" {
 }
 resource "azurerm_subnet" "subnetB" {
   name                 = var.azurerm_subnetB
-  resource_group_name  = data.terraform_remote_state.resource_group_state.outputs.resource_group_name
+  resource_group_name  = var.vnet_rg_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.azurerm_subnet_subnetB_address_prefixes]
   depends_on = [
--- Virtual_Network/Development/variables.tf
+++ Virtual_Network/Development/variables.tf
@@ -10,6 +10,9 @@ variable "vnet_storage_account_name" {
 variable "vnet_storage_account_container_name" {
   type        = string
 }
+variable "vnet_rg_name" {
+  type        = string
+}
 variable "vnet_storage_account_key" {
   type        = string
 }
